00c9b2e3062c521663a0c81f57de65be3c988cf9
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-04-11 15:49:59 +0530

    Create db.py

---
+++ utils/db.py
@@ -0,0 +1,36 @@
+from supabase import create_client
+
+SUPABASE_URL = "YOUR_URL"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthbWdrd2ZrenZvZmt3ZXhocnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4NzA3MjAsImV4cCI6MjA5MTQ0NjcyMH0.8I3OgpamJBjw65ifO9djPKwFv0SwgO92UYvrXTYq5U0"
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# EVENTS
+def create_event(data):
+    return supabase.table("events").insert(data).execute()
+
+def get_events():
+    return supabase.table("events").select("*").execute()
+
+def delete_event(event_id):
+    return supabase.table("events").delete().eq("id", event_id).execute()
+
+# USERS
+def create_user(data):
+    return supabase.table("users").insert(data).execute()
+
+def get_users():
+    return supabase.table("users").select("*").execute()
+
+# APPLICATIONS
+def apply_event(data):
+    return supabase.table("applications").insert(data).execute()
+
+def get_applications():
+    return supabase.table("applications").select("*").execute()
+
+def update_status(id, status, qr=None):
+    return supabase.table("applications").update({
+        "status": status,
+        "qr_code": qr
+    }).eq("id", id).execute()
