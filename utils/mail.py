f574db62b426784788b7d080b59e3f88e96f9b34
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-04-14 09:44:04 +0530

    Update mail.py

--- utils/mail.py
+++ utils/mail.py
@@ -4,18 +4,29 @@ from email.message import EmailMessage
 def send_qr(to_email, file_path):
 
     EMAIL = "abhitheboss2004@gmail.com"
-    PASSWORD = "vdso vsez dgzm jkgd"
+    PASSWORD = "vdso vsez dgzm jkgd"  # must be APP PASSWORD
 
-    msg = EmailMessage()
-    msg["Subject"] = "Your Event Pass"
-    msg["From"] = EMAIL
-    msg["To"] = to_email
+    try:
+        msg = EmailMessage()
+        msg["Subject"] = "🎟️ Your Event Pass"
+        msg["From"] = EMAIL
+        msg["To"] = to_email
 
-    msg.set_content("Your QR Pass attached")
+        msg.set_content("Your registration is approved. QR pass attached.")
 
-    with open(file_path, "rb") as f:
-        msg.add_attachment(f.read(), maintype="image", subtype="png", filename="qr.png")
+        with open(file_path, "rb") as f:
+            msg.add_attachment(
+                f.read(),
+                maintype="image",
+                subtype="png",
+                filename="qr.png"
+            )
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(EMAIL, PASSWORD)
-        smtp.send_message(msg)
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(EMAIL, PASSWORD)
+            smtp.send_message(msg)
+
+        print("✅ MAIL SENT")
+
+    except Exception as e:
+        print("❌ MAIL ERROR:", e)
